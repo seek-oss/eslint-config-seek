72c350ebbd41ec395390a5f6f4bcd5f6d834d10e
Ben <bjervis@seek.com.au>
2020-01-13 13:04:13 +1100

    Make todos not an error

--- .eslintrc.js
+++ .eslintrc.js
@@ -42,7 +42,6 @@ const baseRules = {
   'no-throw-literal': ERROR,
   'no-useless-call': ERROR,
   'no-void': ERROR,
-  'no-warning-comments': ERROR,
   radix: ERROR,
   'vars-on-top': ERROR,
   yoda: ERROR,
@@ -142,6 +141,7 @@ const baseConfig = {
         // prefer TypeScript exhaustiveness checking
         // https://www.typescriptlang.org/docs/handbook/advanced-types.html#exhaustiveness-checking
         'default-case': OFF,
+        'no-warning-comments': OFF,
       },
     },
     {
