f6da8373e4704431c29cfbe3ddf8237a4f9c6485
Ryan Ling <ryan@outlook.com.au>
2021-10-26 13:13:35 +1100

    Migrate to Changesets (#63)
    It's looking like our config will require numerous breaking changes to
    get to ESLint v8 compatibility (#62). If we stuck with semantic-release
    we'd either need to get creative with branching, have one mega PR or
    release a bunch of intermediary major versions that add little value.
    
    Instead, switch to Changesets. This is used in a number of our OSS repos
    and makes it easier to bundle multiple changes into a release. We
    support previewing the release notes with and without a `GITHUB_TOKEN`
    via `yarn version`.

---
+++ .changeset/changelog.js
@@ -0,0 +1,162 @@
+const {
+  getInfo,
+  getInfoFromPullRequest,
+} = require('@changesets/get-github-info');
+
+/**
+ * Adapted from `@changesets/cli`.
+ *
+ * {@link https://github.com/atlassian/changesets/blob/%40changesets/cli%402.17.0/packages/cli/src/changelog/index.ts}
+ *
+ * @type import('@changesets/types').ChangelogFunctions
+ */
+const defaultChangelogFunctions = {
+  getDependencyReleaseLine: async (changesets, dependenciesUpdated) => {
+    if (dependenciesUpdated.length === 0) return '';
+
+    const changesetLinks = changesets.map(
+      (changeset) => `- Updated dependencies [${changeset.commit}]`,
+    );
+
+    const updatedDependenciesList = dependenciesUpdated.map(
+      (dependency) => `  - ${dependency.name}@${dependency.newVersion}`,
+    );
+
+    return [...changesetLinks, ...updatedDependenciesList].join('\n');
+  },
+  getReleaseLine: async (changeset) => {
+    const [firstLine, ...futureLines] = changeset.summary
+      .split('\n')
+      .map((l) => l.trimRight());
+
+    const suffix = changeset.commit;
+
+    return `\n\n- ${firstLine}${suffix ? ` (${suffix})` : ''}\n${futureLines
+      .map((l) => `  ${l}`)
+      .join('\n')}`;
+  },
+};
+
+/**
+ * Adapted from `@changesets/changelog-github`.
+ *
+ * {@link https://github.com/atlassian/changesets/blob/%40changesets/changelog-github%400.4.1/packages/changelog-github/src/index.ts}
+ *
+ * @type import('@changesets/types').ChangelogFunctions
+ */
+const gitHubChangelogFunctions = {
+  getDependencyReleaseLine: async (
+    changesets,
+    dependenciesUpdated,
+    options,
+  ) => {
+    if (!options.repo) {
+      throw new Error(
+        'Please provide a repo to this changelog generator like this:\n"changelog": ["./changelog.js", { "repo": "org/repo" }]',
+      );
+    }
+    if (dependenciesUpdated.length === 0) return '';
+
+    const changesetLink = `- Updated dependencies [${(
+      await Promise.all(
+        changesets.map(async (cs) => {
+          if (cs.commit) {
+            let { links } = await getInfo({
+              repo: options.repo,
+              commit: cs.commit,
+            });
+            return links.commit;
+          }
+        }),
+      )
+    )
+      .filter((_) => _)
+      .join(', ')}]:`;
+
+    const updatedDependenciesList = dependenciesUpdated.map(
+      (dependency) => `  - ${dependency.name}@${dependency.newVersion}`,
+    );
+
+    return [changesetLink, ...updatedDependenciesList].join('\n');
+  },
+  getReleaseLine: async (changeset, _type, options) => {
+    if (!options || !options.repo) {
+      throw new Error(
+        'Please provide a repo to this changelog generator like this:\n"changelog": ["./changelog.js", { "repo": "org/repo" }]',
+      );
+    }
+
+    /** @type number | undefined */
+    let prFromSummary;
+    /** @type string | undefined */
+    let commitFromSummary;
+
+    const replacedChangelog = changeset.summary
+      .replace(/^\s*(?:pr|pull|pull\s+request):\s*#?(\d+)/im, (_, pr) => {
+        let num = Number(pr);
+        if (!isNaN(num)) prFromSummary = num;
+        return '';
+      })
+      .replace(/^\s*commit:\s*([^\s]+)/im, (_, commit) => {
+        commitFromSummary = commit;
+        return '';
+      })
+      .replace(/^\s*(?:author|user):\s*@?([^\s]+)/gim, (_, user) => {
+        usersFromSummary.push(user);
+        return '';
+      })
+      .trim();
+
+    const [firstLine, ...futureLines] = replacedChangelog
+      .split('\n')
+      .map((l) => l.trimRight());
+
+    const links = await (async () => {
+      if (prFromSummary !== undefined) {
+        let { links } = await getInfoFromPullRequest({
+          repo: options.repo,
+          pull: prFromSummary,
+        });
+        if (commitFromSummary) {
+          links = {
+            ...links,
+            commit: `[\`${commitFromSummary}\`](https://github.com/${options.repo}/commit/${commitFromSummary})`,
+          };
+        }
+        return links;
+      }
+      const commitToFetchFrom = commitFromSummary || changeset.commit;
+      if (commitToFetchFrom) {
+        let { links } = await getInfo({
+          repo: options.repo,
+          commit: commitToFetchFrom,
+        });
+        return links;
+      }
+      return {
+        commit: null,
+        pull: null,
+        user: null,
+      };
+    })();
+
+    const suffix = links.pull ?? links.commit;
+
+    return [
+      `\n- ${firstLine}${suffix ? ` (${suffix})` : ''}`,
+      ...futureLines.map((l) => `  ${l}`),
+    ].join('\n');
+  },
+};
+
+if (process.env.GITHUB_TOKEN) {
+  module.exports = gitHubChangelogFunctions;
+} else {
+  console.warn(
+    `Defaulting to Git-based versioning.
+Enable GitHub-based versioning by setting the GITHUB_TOKEN environment variable.
+This requires a GitHub personal access token with the \`public_repo\` scope: https://github.com/settings/tokens/new`,
+  );
+
+  module.exports = defaultChangelogFunctions;
+}
